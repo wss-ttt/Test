271ee36b6a4e70b82b7b3b25c8253f78952a732e
USER-8LNJD3T32B\Administrator <602466011@qq.com>
2020-04-01 17:02:41 +0800

    添加：index5

---
+++ vue-test/001/src/views/index5.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  components: {},
+  created() {},
+  mounted() {},
+  destroyed() {},
+  watch: {},
+  methods: {}
+}
+</script>
+<style scoped>
+.box {
+  width: 200px;
+  height: 100px;
+  background: yellow;
+}
+</style>
